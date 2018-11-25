e44086cb88d6279c97d0bff75cc09b9f00832dbf
Matt Holloway <hollowmatt@gmail.com>
2018-11-25 18:09:36 -0500

    Add single question and button

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 // JS for the quiz
+function question() {
+	const question = "What is your favorite book?"
+	const answer = prompt(question);
+	alert(`You answered ${answer}`);
+}
+const startQuiz = document.querySelector('.startQuiz');
+startQuiz.addEventListener('click', question);
