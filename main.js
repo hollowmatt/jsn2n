8cf86a1fcd17ada787cec875137a99e18527aa11
Matt Holloway <hollowmatt@gmail.com>
2018-11-25 18:38:46 -0500

    Add some more questions, start keeping track

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 // JS for the quiz
 function question() {
-	const question = "What is your favorite book?"
-	const answer = prompt(question);
-	alert(`You answered ${answer}`);
+	const  quiz = [
+								 ["What is Superman's real name?", "Clark Kent"],
+								 ["What is Wonder Woman's real name?", "Diana Prince"],
+								 ["What is Batman's real name?", "Bruce Wayne"]
+								];
+	let score = 0;
+	for([question,answer] of quiz) {
+		const response = prompt(question);
+		if(response === answer) {
+			score++;
+		}
+	}
+
+	alert(`You answered ${score} questions correctly.`);
 }
 const startQuiz = document.querySelector('.startQuiz');
 startQuiz.addEventListener('click', question);
