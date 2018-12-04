5f86d092a9d83edb9468d06d7289da0574a6be2b
Matt Holloway <hollowmatt@gmail.com>
2018-12-03 19:35:55 -0500

    refactoring the image display logic in JS

--- images.js
+++ images.js
@@ -1,21 +1,21 @@
 // JS for the quiz
 (function() {
   //Questions we'll ask
-  const images = [
+  const myImages = [
     {
       images: {
         a: "images/01_beach.jpeg",
         b: "images/01_gym.jpeg",
         c: "images/01_movie.jpeg",
-        c: "images/01_reading.jpeg"
+        d: "images/01_reading.jpeg"
       }
     },
     {
       images: {
-        a: "images/02_winger.jpeg",
+        a: "images/02_winter.jpeg",
         b: "images/02_summer.jpeg",
         c: "images/02_spring.jpeg",
-        c: "images/02_fall.jpeg"
+        d: "images/02_fall.jpeg"
       }
     },
     {
@@ -23,7 +23,7 @@
         a: "images/03_cat.jpeg",
         b: "images/03_dog.jpeg",
         c: "images/03_bird.jpeg",
-        c: "images/03_snake.jpeg"
+        d: "images/03_snake.jpeg"
       }
     }
   ];
@@ -62,53 +62,38 @@
   //Function to build the image boxes from the JSON data
   function buildImages() {
     const output = [];
-    // for each question...
-    images.forEach((currentQuestion, questionNumber) => {
-      // we'll want to store the list of answer choices
-      const answers = [];
-      // and for each available answer...
-      for (letter in currentQuestion.answers) {
-        // ...add an HTML radio button
-        answers.push(
-          `<label>
-             <input type="radio" name="question${questionNumber}" value="${letter}">
-              ${letter} :
-              ${currentQuestion.answers[letter]}
-           </label>`
+    myImages.forEach((currentSet, answerNumber) => {
+      const selections = [];
+      for(letter in currentSet.images) {
+        console.log(`${answerNumber} : ${letter} : ${currentSet.images[letter]}`);
+        //build the grid
+        selections.push(
+          `<div class="box">
+            <div class="boxInner">
+              <img id='${answerNumber}_${letter}'src='${currentSet.images[letter]}' />
+            </div>
+          </div>`
         );
       }
-      // add this question and its answers to the output
+      //add to output
       output.push(
-        `<div class="slide">
-           <div class="question"> ${currentQuestion.question} </div>
-           <div class="answers"> ${answers.join("")} </div>
-         </div>`
+        `<div class="wrap">
+          ${selections.join("")}
+        </div>` 
       );
     });
-
-    // finally combine our output list into one string of HTML and put it on the page
-    quizContainer.innerHTML = output.join("");
+    imageContainer.innerHTML = output.join("");
   }
 
   //Function to show the recommendation based on the answer key
   function showRecommendation() {
-    // gather answer containers from our quiz
-    const answerContainers = quizContainer.querySelectorAll(".answers");
-
-    let answerKey = "";
-    // for each question...
-    myQuestions.forEach((currentQuestion, questionNumber) => {
-      // find selected answer
-      const answerContainer = answerContainers[questionNumber];
-      const selector = `input[name=question${questionNumber}]:checked`;
-      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-      answerKey += userAnswer;
-    });
+    
     console.log(answerKey);
     recommendation = findBook(answerKey, recommendations);
     resultsContainer.innerHTML = `You should really read: <br/><div class="title"> ${recommendation} </div>`;
   }
 
+  //Function to find a book in the array of answers based on answer given
   function findBook(key, value) {
     var i, len = value.length;
     
@@ -121,45 +106,24 @@
     return -1;
   }
 
-  //This will show a question
+  //This will show a set of images
   function showSlide(n) {
-    slides[currentSlide].classList.remove("active-slide");
-    slides[n].classList.add("active-slide");
-    currentSlide = n;
-    
-    if (currentSlide === 0) {
-      previousButton.style.display = "none";
-    } else {
-      previousButton.style.display = "inline-block";
-    }
-    
-    if (currentSlide === slides.length - 1) {
-      nextButton.style.display = "none";
-      submitButton.style.display = "inline-block";
-    } else {
-      nextButton.style.display = "inline-block";
-      submitButton.style.display = "none";
-    }
+
   }
 
-  //show next question
+  //show next image set
   function showNextSlide() {
     showSlide(currentSlide + 1);
   }
+ 
 
-  //show previous question
-  function showPreviousSlide() {
-    showSlide(currentSlide - 1);
-  }
-
-  const quizContainer = document.getElementById("quiz");
+  const imageContainer = document.getElementById("image-container");
   const resultsContainer = document.getElementById("results");
   const submitButton = document.getElementById("submit");
 
-  // display quiz right away
-  buildQuiz();
+  // display first set of images right away
+  buildImages();
 
-  const previousButton = document.getElementById("previous");
   const nextButton = document.getElementById("next");
   const slides = document.querySelectorAll(".slide");
   let currentSlide = 0;
@@ -168,6 +132,5 @@
 
   // on submit, show recommendations
   submitButton.addEventListener("click", showRecommendation);
-  previousButton.addEventListener("click", showPreviousSlide);
   nextButton.addEventListener("click", showNextSlide);
 })();
